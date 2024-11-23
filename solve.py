5b8f31a445c2ed3cbe5cf722f602c27250cd9662
AMIO2TR <155319834+AMIO2TR@users.noreply.github.com>
2024-11-24 01:05:13 +0600

    Delete solve.py

--- solve.py
+++
@@ -1,9 +0,0 @@
-#decrypt the .pem file (rsa_key.d use for decrypt the number )
-
-import Crypto
-from Crypto.PublicKey import RSA
-
-with open("private.pem", "rb") as f:
-    key = f.read()
-    rsa_key = RSA.importKey(key)
-    print(rsa_key.d)
